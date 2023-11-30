16780540b67b675916da96fa76c495e202a88c32
arimura <33759663+arimura@users.noreply.github.com>
2023-11-30 17:45:31 +0900

    Retrive dep files

--- main.go
+++ main.go
@@ -14,7 +14,6 @@ func main() {
 	scanner.Scan()
 	filePath := scanner.Text()
 
-	fmt.Println(filePath)
 	content, err := ioutil.ReadFile(filePath)
 	if err != nil {
 		fmt.Println("Error reading file:", err)
@@ -22,11 +21,12 @@ func main() {
 	}
 
 	// Dummy implementation: Replace this with actual logic to parse Java file and find dependencies
-	dependencies := findDependencies(string(content))
+	dependencies := findDependencies(string(content), filePath)
 
 	// Read and combine dependencies
 	var combinedContent string
 	for _, dep := range dependencies {
+		// println(dep)
 		depContent, err := ioutil.ReadFile(dep)
 		if err != nil {
 			fmt.Println("Error reading dependency file:", err)
@@ -38,11 +38,44 @@ func main() {
 	combinedContent += string(content)
 
 	// Output the combined content
-	fmt.Println(combinedContent)
+	// fmt.Println(combinedContent)
 }
 
-func findDependencies(content string) []string {
-	// Dummy implementation: Replace with actual logic to parse and find class dependencies
-	// This is just a placeholder
-	return strings.Fields(content)
+func findDependencies(content string, filePath string) []string {
+	var dependencies []string
+
+	lines := strings.Split(content, "\n")
+	packageName := ""
+	for _, line := range lines {
+		if strings.HasPrefix(line, "package ") {
+			packageName = strings.TrimPrefix(line, "package ")
+			packageName = strings.TrimSuffix(packageName, ";")
+			break
+		}
+	}
+	if packageName == "" {
+		panic("package name not found: " + filePath)
+	}
+
+	packagePath := strings.ReplaceAll(packageName, ".", "/")
+	index := strings.LastIndex(filePath, packagePath)
+	if index == -1 {
+		panic("failed to find package root path: " + filePath)
+	}
+	packageRootDirPath := filePath[:index]
+
+	for _, line := range lines {
+		if strings.HasPrefix(line, "import ") {
+			importLine := strings.TrimPrefix(line, "import ")
+			importLine = strings.TrimSuffix(importLine, ";")
+			importLine = strings.ReplaceAll(importLine, ".", "/")
+			fp := packageRootDirPath + importLine + ".java"
+
+			if _, err := os.Stat(fp); err == nil {
+				dependencies = append(dependencies, fp)
+			}
+		}
+	}
+
+	return dependencies
 }
