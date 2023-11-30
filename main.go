ba34cc844e1e948ddbe3c63ed56272648719b8dc
arimura <33759663+arimura@users.noreply.github.com>
2023-11-30 18:35:03 +0900

    Refactor

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
@@ -20,7 +21,10 @@ func main() {
 	}
 	targetFile := string(content)
 
-	dependencies := findDependencies(targetFile, filePath)
+	dependencies, err := findDependencies(targetFile, filePath)
+	if err != nil {
+		panic(err)
+	}
 
 	template := ""
 	if _, err := os.Stat("./template/default.txt"); err == nil {
@@ -41,41 +45,71 @@ func main() {
 	}
 }
 
-func findDependencies(content string, filePath string) []string {
-	var dependencies []string
-
+// findDependencies parses the Java file content and returns file paths
+// of all dependencies that are part of the same package.
+func findDependencies(content string, filePath string) ([]string, error) {
 	lines := strings.Split(content, "\n")
-	packageName := ""
+	packageName, err := extractPackageName(lines)
+	if err != nil {
+		return nil, fmt.Errorf("extracting package name: %w", err)
+	}
+
+	packageRootDirPath, err := findPackageRootDirPath(filePath, packageName)
+	if err != nil {
+		return nil, fmt.Errorf("finding package root directory path: %w", err)
+	}
+
+	return parseImports(lines, packageRootDirPath), nil
+}
+
+// extractPackageName extracts the package name from the given lines of a Java file.
+func extractPackageName(lines []string) (string, error) {
 	for _, line := range lines {
 		if strings.HasPrefix(line, "package ") {
-			packageName = strings.TrimPrefix(line, "package ")
-			packageName = strings.TrimSuffix(packageName, ";")
-			break
+			packageName := strings.TrimPrefix(line, "package ")
+			return strings.TrimSuffix(packageName, ";"), nil
 		}
 	}
-	if packageName == "" {
-		panic("package name not found: " + filePath)
-	}
+	return "", fmt.Errorf("package name not found")
+}
 
+// findPackageRootDirPath finds the root directory path of the package in the file path.
+func findPackageRootDirPath(filePath, packageName string) (string, error) {
 	packagePath := strings.ReplaceAll(packageName, ".", "/")
 	index := strings.LastIndex(filePath, packagePath)
 	if index == -1 {
-		panic("failed to find package root path: " + filePath)
+		return "", fmt.Errorf("package root path not found")
 	}
-	packageRootDirPath := filePath[:index]
+	return filePath[:index], nil
+}
+
+// parseImports processes the provided lines from a Java file, identifies import statements,
+// and returns a slice of file paths for those imports that are from the same package.
+func parseImports(lines []string, packageRootDirPath string) []string {
+	var dependencies []string
 
 	for _, line := range lines {
-		if strings.HasPrefix(line, "import ") {
-			importLine := strings.TrimPrefix(line, "import ")
-			importLine = strings.TrimSuffix(importLine, ";")
-			importLine = strings.ReplaceAll(importLine, ".", "/")
-			fp := packageRootDirPath + importLine + ".java"
+		// Skip non-import lines quickly
+		if !strings.HasPrefix(line, "import ") {
+			continue
+		}
 
-			if _, err := os.Stat(fp); err == nil {
-				dependencies = append(dependencies, fp)
-			}
+		// Process import line
+		importPath := extractImportPath(line)
+		fullPath := filepath.Join(packageRootDirPath, importPath) + ".java"
+
+		// Check if file exists and add to dependencies if it does
+		if _, err := os.Stat(fullPath); err == nil {
+			dependencies = append(dependencies, fullPath)
 		}
 	}
 
 	return dependencies
+}
+
+// extractImportPath takes an import line from a Java file and converts it into a file path.
+func extractImportPath(importLine string) string {
+	importLine = strings.TrimPrefix(importLine, "import ")
+	importLine = strings.TrimSuffix(importLine, ";")
+	return strings.ReplaceAll(importLine, ".", "/")
 }
